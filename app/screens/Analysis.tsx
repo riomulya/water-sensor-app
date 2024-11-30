81d7f1624991d9864d831bc12640e15f0caa3a8f
riomulya <riomulya75@gmail.com>
2024-11-30 15:29:33 +0700

    update

--- app/screens/Analysis.tsx
+++ app/screens/Analysis.tsx
@@ -1,9 +1,12 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import React from 'react';
 import { Input, InputField } from '@/components/ui/input';
 import { Card } from '@/components/ui/card';
 import { Heading } from '@/components/ui/heading';
 import { Text } from '@/components/ui/text';
+import { Image } from 'react-native';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { Link } from '@/components/ui/link';
 
 const AnalysisScreen = () => {
     const data = [
@@ -31,11 +34,27 @@ const AnalysisScreen = () => {
             date: 'Monday, 21-5-2024',
             image: 'https://via.placeholder.com/150',
         },
+        {
+            title: 'Sungai Barito',
+            location: 'Jl. Raya Puspiptek, Buaran...',
+            date: 'Monday, 21-5-2024',
+            image: 'https://via.placeholder.com/150',
+        }, {
+            title: 'Sungai Barito',
+            location: 'Jl. Raya Puspiptek, Buaran...',
+            date: 'Monday, 21-5-2024',
+            image: 'https://via.placeholder.com/150',
+        }, {
+            title: 'Sungai Barito',
+            location: 'Jl. Raya Puspiptek, Buaran...',
+            date: 'Monday, 21-5-2024',
+            image: 'https://via.placeholder.com/150',
+        },
     ];
 
     return (
         <View style={styles.container}>
-            <View style={{ ...styles.searchBox }} >
+            <View style={{ ...styles.searchBox, zIndex: 1 }} >
                 <View style={{ ...styles.inputContainer }}>
                     <Input variant="outline" size="md" isDisabled={false} isInvalid={false} isReadOnly={false} className='flex-1'  >
                         <InputField
@@ -44,14 +63,30 @@ const AnalysisScreen = () => {
                     </Input>
                 </View>
             </View>
-
-            <Card size="lg" variant="outline" className="m-3">
-                <Heading size="md" className="mb-1">
-                    Quick Start
-                </Heading>
-                <Text size="sm">Start building your next project in minutes</Text>
-            </Card>
-
+            <ScrollView contentContainerStyle={styles.scrollViewContainer}>
+                {data.map((item, index) => (
+                    <Card size="lg" variant="outline" className={`m-3 ${index === data.length - 1 ? 'mb-28' : ''}`}
+                    >
+                        <View className='flex flex-row'>
+                            <Image
+                                source={require('../../assets/images/Nature.png')}
+                                // className="mb-6 h-[240px] rounded-md"
+                                alt="Nature"
+                            />
+                            <View className='ml-3'>
+                                <Heading size="lg" className="mb-1">
+                                    {item.title}
+                                </Heading>
+                                <Text size="sm" className='color-gray-400' >{item.location}</Text>
+                                <Text size="sm" className='color-gray-400'>{item.date}</Text>
+                                <Link>
+                                    <Text size="lg" className='color-red-600'>Cek Selengkapnya <FontAwesome6 name="arrow-right-from-bracket" size={15}></FontAwesome6></Text>
+                                </Link>
+                            </View>
+                        </View>
+                    </Card>
+                ))}
+            </ScrollView>
         </View >
     );
 };
@@ -109,5 +144,9 @@ const styles = StyleSheet.create({
         padding: 10,
         borderBottomColor: '#ccc',
         borderBottomWidth: 1,
+    },
+    scrollViewContainer: {
+        paddingTop: 80, // Memberikan padding di atas untuk mencegah konten tertutup oleh search bar
+        paddingBottom: 20, // Menambahkan ruang bawah agar lebih nyaman scrollnya
     },
 });
--- app/screens/Settings.tsx
+++ app/screens/Settings.tsx
@@ -1,11 +1,56 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
 import React, { Component } from 'react'
+import { Card } from '@/components/ui/card'
+import { Heading } from '@/components/ui/heading'
+import { Progress, ProgressFilledTrack } from '@/components/ui/progress'
+import { VStack } from '@/components/ui/vstack'
+import { Text } from '@/components/ui/text'
+
+const data = [
+    {
+        title: 'Accel X',
+        value: 46,
+        unit: 'm/s^2'
+    }, {
+        title: 'Accel Y',
+        value: 46,
+        unit: 'm/s^2'
+    }, {
+        title: 'Accel Z',
+        value: 46,
+        unit: 'm/s^2'
+    }, {
+        title: 'pH',
+        value: 46,
+        unit: 'pH'
+    }, {
+        title: 'Temperature',
+        value: 6,
+        unit: '°C'
+    }, {
+        title: 'Turbidity',
+        value: 46,
+        unit: 'NTU'
+    },
+]
 
 export default class Settings extends Component {
+
     render() {
         return (
             <View>
-                <Text>Settings</Text>
+                <Card size="lg" variant="outline" className='m-5'
+                >
+                    {data.map((item, index) => (
+                        <VStack space="lg" key={index}>
+                            <Heading>{item.title}</Heading>
+                            <Progress value={item.title === "pH" ? Math.round((Number(item.value / 14)) * 100) : Number(item.value)} className="w-96 h-2" size="sm">
+                                <ProgressFilledTrack className="bg-red-600" />
+                            </Progress>
+                            <Text size="md">{item.title === "pH" ? Math.round((Number(item.value / 14)) * 100) : Number(item.value) + ' ' + item.unit}</Text>
+                        </VStack>
+                    ))}
+                </Card>
             </View>
         )
     }
